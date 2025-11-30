400fa4f17b149d89f5bdb923658b6e2ee3842c39
Ivan Zenkovich <ivanzenkovich@gmail.com>
2025-11-30 02:12:40 +0100

    preparing for git pages

--- vite.config.ts
+++ vite.config.ts
@@ -3,9 +3,8 @@ import react from "@vitejs/plugin-react-swc";
 import path from "path";
 import { componentTagger } from "lovable-tagger";
 
-// https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
-  base: "/pdf-billing-form-builder/",
+  base: mode === "production" ? "/pdf-billing-form-builder/" : "/",
   server: {
     host: "::",
     port: 8080,
